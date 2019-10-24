f8f83657a2752699c220d7c72d42938b08bb3aca
Fawaz Ahmed <52136572+Fxa180017@users.noreply.github.com>
2019-10-24 13:16:19 -0500

    Add files via upload

--- twittersearcher.py
+++ twittersearcher.py
@@ -1,4 +1,4 @@
-import tweepy, schedule, time, json
+import tweepy, schedule, time, json, pymongo
 from pprint import pprint
 from geopy.geocoders import Nominatim
 numInc = 0
@@ -7,7 +7,6 @@ accessToken = "3939721633-EJIfdOWAcuWVjwFpsQp7Qs3P5NNyh3q7ihpC9ld"
 accessToken_Secret = "wyQ9uA8vZ02Ni2fimTJegOSdIJTO95ttIJWWkDZUxl5cZ"
 apiKey = "8VQzfyd9REr8gO2nesBRkJTru"
 apiKey_Secret = "2CS4iYIdBq6SNSmTJCWUFJuhOjAkN4b68bXkOIS7nYouuh7r8n"
-
 
 def authenticate(): #authenticates with tweepy
 	global accessToken, accesssToken_Secret, apiKey, apiKey_Secert
@@ -21,31 +20,34 @@ def authenticate(): #authenticates with tweepy
 def compareResults(results_without_location, results_with_location):
 	global radius
 	i = 0
-	results_no_location = {}
-	results_location = {}
+	tweets_set1 = []
+	tweets_set2 = []
 	tweetedIn = {'Tweeted within' : str(radius) + ' miles'}				#either one of these two dicts will be updated to the first set of tweets
-	tweetedOut = {'Not tweeted within' : str(radius) + 'miles'}
+	tweetedOut = {'Not tweeted within' : str(radius) + ' miles'}
 	for tweet in results_without_location: 						#stores tweets with out location in a dict
 		tweets = {'Username' : tweet.user.screen_name, 'Tweet' : tweet.text}
-		results_no_location[i] = {'Tweet' + str((i+1)) : tweets}
+		tweets_set1.append(tweets)
 		i+=1
 	i = 0
 	for tweet in results_with_location:						#same thing here but with tweets with a location
 		tweets = {'Username' : tweet.user.screen_name, 'Tweet' : tweet.text}
-		results_location[i] = {'Tweet' + str((i+1)) : tweets}
+		tweets_set2.append(tweets)
 		i+=1
-	for x in results_no_location:
-		for y in results_location:
-			if results_no_location[x] == results_location[y]:		#if a tweet that used only the search query is found within the radius of the coordinates then it updates its dict
-				results_no_location[x].update(tweetedIn)
+	len1 = len(tweets_set1)
+	len2 = len(tweets_set2)
+	for x in range(len1):
+		for y in range(len2):
+			if tweets_set1[x] == tweets_set2[y]:
+				tweets_set1[x].update(tweetedIn)
 				break
 			else:
-				results_no_location[x].update(tweetedOut)
-	storeResults(results_no_location)
+				tweets_set1[x].update(tweetedOut)
+	storeResults(tweets_set1)
 
 def storeResults(results): #Stores into a json
+	length = len(results)
 	with open('output.json', 'w') as file:
-		for x in results:
+		for x in range(length):
 			json.dump(results[x], file)
 			file.write('\n')
 
@@ -84,8 +86,7 @@ if __name__ == '__main__':
 #	long = input("Enter longitude: ")
 	lati = 6.5244
 	long = 3.3792 #lagos coordinates
-	searchQuery = geoLocator(lati,long) + " Accident -filter:retweets" 	#-filter:retweets removes retweets
-#	searchQuery = "Lagos Accident -filter:retweets"
+#	searchQuery = geoLocator(lati,long) + " Accident -filter:retweets" 	#-filter:retweets removes retweets
+	searchQuery = "Lagos Accident -filter:retweets"
 	twitterSearcher(lati, long, searchQuery) 				#remove or comment this line and just used the schedule function to start the program
-	iterations = int(input("Enter how many times before schedule stops "))
-	scheduleIteration(lati, long, searchQuery, iterations)
+#	iterations = int(input("Enter how many times before schedule stops "))
